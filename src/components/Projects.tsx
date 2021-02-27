f86eccaec2698ec531cc6e47d6195108ab86eb41
John Letey <johnletey@gmail.com>
2021-02-27 11:27:42 +0000

    chore: extract projects into a component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,41 @@
+import Block from "./Block";
+
+const Data = [
+  {
+    name: "KYVE",
+    description: "The unified archive for blockchains.",
+    link: "https://kyve.network",
+  },
+  {
+    name: "th8ta",
+    description: "Building from all angles.",
+    link: "https://th8ta.org",
+  },
+  {
+    name: "Verto",
+    description: "A decentralised trading protocol on Arweave.",
+    link: "https://verto.exchange",
+  },
+  {
+    name: "ArVerify",
+    description: "Trusted verification for the Arweave network.",
+    link: "https://arverify.org",
+  },
+];
+
+const Projects = () => {
+  return (
+    <>
+      {Data.map((project) => (
+        <Block
+          name={project.name}
+          description={project.description}
+          link={project.link}
+          key={project.name}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Projects;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,10 @@
 import { Page } from "@geist-ui/react";
-import Block from "../components/Block";
-
-const Projects = [
-  {
-    name: "KYVE",
-    description: "The unified archive for blockchains.",
-    link: "https://kyve.network",
-  },
-  {
-    name: "th8ta",
-    description: "Building from all angles.",
-    link: "https://th8ta.org",
-  },
-  {
-    name: "Verto",
-    description: "A decentralised trading protocol on Arweave.",
-    link: "https://verto.exchange",
-  },
-  {
-    name: "ArVerify",
-    description: "Trusted verification for the Arweave network.",
-    link: "https://arverify.org",
-  },
-];
+import Projects from "../components/Projects";
 
 const Home = () => {
   return (
     <Page>
-      {Projects.map((project) => (
-        <Block
-          name={project.name}
-          description={project.description}
-          link={project.link}
-          key={project.name}
-        />
-      ))}
+      <Projects />
     </Page>
   );
 };
